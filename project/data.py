b18740fabb2e72766189a28bc40f802a3904b721
MabeDiaby <diabymabe@gmail.com>
2022-12-02 23:50:53 -0500

    Triva questions

---
+++ project/data.py
@@ -0,0 +1,82 @@
+question_data = [
+    {
+        "category":"Entertainment: Books",
+        "type":"boolean",
+        "difficulty":"easy",
+        "question":"The book 1984 was published in 1949.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "category":"Entertainment: Video Games",
+        "type":"boolean",
+        "difficulty":"hard",
+        "question":"In the game &quot;Melty Blood Actress Again Current Code&quot;, you can enter Blood Heat mode in Half Moon style.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {
+        "category":"Entertainment: Video Games",
+        "type":"boolean",
+        "difficulty":"easy",
+        "question":"In &quot;Sonic Adventure&quot;, you are able to transform into Super Sonic at will after completing the main story.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {
+        "category":"Mythology",
+        "type":"boolean",
+        "difficulty":"easy",
+        "question":"According to Greek Mythology, Zeus can control lightning.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "category":"Entertainment: Video Games",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"In Rocket League, you can play Basketball.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "category":"Entertainment: Video Games",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"In the video game &quot;Transistor&quot;, &quot;Red&quot; is the name of the main character.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "category":"Entertainment: Video Games",
+        "type":"boolean",
+        "difficulty":"easy",
+        "question":"The game &quot;Pocket Morty&quot; has a Morty called &quot;Pocket Mortys Morty&quot;?",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "category":"Entertainment: Video Games",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"&quot;Return to Castle Wolfenstein&quot; was the only game of the Wolfenstein series where you don&#039;t play as William &quot;B.J.&quot; Blazkowicz.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {
+        "category":"Entertainment: Japanese Anime & Manga",
+        "type":"boolean",
+        "difficulty":"hard",
+        "question":"In the &quot;Kagerou Daze&quot; series, Shintaro Kisaragi is prominently shown with the color red.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "category":"Entertainment: Video Games",
+        "type":"boolean",
+        "difficulty":"easy",
+        "question":"In Until Dawn, both characters Sam and Mike cannot be killed under any means until the final chapter of the game.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    }
+]
